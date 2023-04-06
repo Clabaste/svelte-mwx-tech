f5707639bbd9f3bcf7cc9092afd867f211a7caa1
bastert <bastert@mediaworx.de>
2023-04-06 11:18:41 +0200

    add intro

--- src/lib/Start.svelte
+++ src/lib/Start.svelte
@@ -3,7 +3,40 @@
 </script>
 
 
-<h1>Start</h1>
+<h1>Svelte</h1>
+<p>
+    Svelte ist als privates Projekt von einem Entwickler namens
+    Rich Harris gestartet und wird mittlerweile von seinem Arbeitgeber, der Firma  <a href="https://vercel.com/">Vercel</a>  unterstützt.
+</p>
+<p>
+    Svelte ermöglicht, wie React, Vue, Angular und Konsoreten, 
+    die Entwicklung von Single-Page-Applikationen und nutzt hierfür ebenfalls Komponenten.
+</p>
+<p>
+    Der Aufbau einer Komponente ist ähnlich wie bei Vue. 
+    Script, HTML-Template und Styles sind in der selben Datei und es gibt die Möglichkeit, über Slots
+    den inneren Content zu kontrollieren.
+</p>
+<h2>Uterschiede zu anderen Frameworks</h2>
+<ul>
+    <li>Build-Prozess: Svelte liefert keine Runtime aus, sondern ist ein Compiler. (Weniger Overhead) </li>
+    <li>Svelte benutzt kein virtuelles DOM</li>
+</ul>
+<h2>Nicht neu, aber sehr gut gemacht</h2>
+<ul>
+    <li>
+         <a href="https://svelte.dev/">Exzellentes Tutorial</a>
+    </li>
+    <li>
+        <a href="https://svelte.dev/tutorial/transition">Transitions</a> 
+        und <a href="https://svelte.dev/tutorial/animate">Animationen</a> eingebaut
+    </li>
+    <li>
+        Viele einfache eingebaute Lösungen: Stores, Reaktivität, Kontexte, Slots
+    </li>
+    <li>Macht echt Spaß!</li>
+    <li>Die Erweiterung SvelteKit als "RundumSorglos" Paket für ServerSideRendering, advanced Routing und Ähnliches</li>   
+</ul>
 <p>
     <a href="https://svelte.dev/tutorial/basics">https://svelte.dev/tutorial/basics</a>
 </p>
